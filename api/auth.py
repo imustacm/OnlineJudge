a298f71f8364592c3af713fda50bf9f051647a7d
ChocoliBao <837104975@qq.com>
2018-03-20 09:33:31 +0800

    增加登录插入登录日志表

--- api/auth.py
+++ api/auth.py
@@ -1,8 +1,13 @@
 import time
+from core.logger import logger
 
-from flask import current_app
+from core.db import db
+from models import LoginLog
+from sqlalchemy import exc
+from flask import current_app, request
 from flask_restful import Resource
 from flask_restful import reqparse
+
 import jwt
 
 try:
@@ -38,11 +43,33 @@ class Login(Resource):
         payload = {'exp': exp, 'user': username}  # JSON 数据
         token = jwt.encode(payload, secret_key)
         user = Users.query.filter_by(username=username).first()
+
+        login_log = LoginLog()
+        login_log.user_id = username
+        login_log.create_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        login_log.ip = request.remote_addr
+
+        flag = 0
         if not user or not user.verify_password(password):
             data = Data(data='Username or Password Error.', status=200)
+            login_log.success_flag = False
+        else:
+            flag = 1
+            login_log.success_flag = True
+            key = get_key_to_hash('login', username=username)
+            sentinel.master.setex(key, DAY, pickle.dumps(token))
+            data = Data(data=token.decode('utf8'), status=200).to_response()
+            data.set_cookie('jwt', token)
+
+        try:
+            db.session.add(login_log)
+            db.session.commit()
+        except exc.IntegrityError as e:
+            logger.warn(e)
+            db.session.rollback()
+            db.session.close()
+            
+        if flag == 0:
             return data.to_response()
-        key = get_key_to_hash('login', username=username)
-        sentinel.master.setex(key, DAY, pickle.dumps(token))
-        data = Data(data=token.decode('utf8'), status=200).to_response()
-        data.set_cookie('jwt', token)
-        return data
+        else:
+            return data
